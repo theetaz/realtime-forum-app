544d7edcf84bc754cd4ef7f3033571da49bf27ea
Nipun Theekshana <nipun@parallax.lk>
2019-07-23 18:25:23 +0530

    generate the models, controllers, migrations and factories

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Models\Question;
+use Faker\Generator as Faker;
+
+$factory->define(Question::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

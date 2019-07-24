77612aabc6e788f0b935c064ef6a3c7c1fe068b4
Nipun Theekshana <nipun@parallax.lk>
2019-07-24 15:45:58 +0530

    add automatic slug generating for category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use App\Helper\GlobalHelper;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = $name;
+        $this->attributes['slug'] = GlobalHelper::generateSlug($name, $this->getTable());
+    }
+
+    public function questions()
+    {
+        return $this->belongsToMany(Question::class);
+    }
+
 }

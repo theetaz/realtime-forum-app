e55a2dc5e3aec59976b5455246bed5f7709aa2d3
Nipun Theekshana <nipun@parallax.lk>
2019-07-24 18:14:17 +0530

    add request validation and resource mapper for reply module

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('question', 'QuestionController');
 Route::apiResource('category', 'CategoryController');
+Route::apiResource('{question}/reply', 'ReplyController');

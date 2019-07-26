fad3f5d306d0659913ba257ed4ed77a74b564b64
Nipun Theekshana <nipun@parallax.lk>
2019-07-26 10:56:22 +0530

    add api route for auth controller

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,23 @@ Route::delete('reply/{reply}', 'ReplyController@destroy')->name('reply.destroy')
 Route::post('like/{reply}', 'LikeController@like')->name('like.add');
 
 Route::delete('like/{reply}', 'LikeController@disLike')->name('like.remove');
+
+
+/*
+ * adding auth api routes
+ * used jwt authentication
+ */
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
